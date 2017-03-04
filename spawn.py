3ea9c40185ecd15f97d9ef33ce38cb9ab94c7b12
CBalsier <cecile.balsier@ymail.com>
2017-03-04 17:33:25 +0100

    préparation pour les infos des GD !

--- spawn.py
+++ spawn.py
@@ -14,36 +14,67 @@ class Spawn():
             self.color = default_colors[randint(0,len(default_colors)-1)]
         else:
             self.color = parent.color
-            self.stage = parent.stage +1 # evolution stage : 0 = pixel, 1, 2, 3
+            self.stage = parent.stage +1 # evolution stage : 0 = pixel, 1, 2, 3
         self.points = []
-        print self.color
 
-        #patterns = [
+        self.color = default_colors[0] # for testing purposes !
+        self.stage = randint(0,1) # for testing purposes !
+
+        patterns = [
                 # red stages 0 to 3
-                #[
-                   #[[0,0]],
-                   #[[0,0],[0,1],[1,0],[0,-1],[-1,0]]
-                   #[[0,0],[0,1],[1,0],[0,-1],[-1,0],[0,2],[2,0],[0,-2],[-2,0]],
-                   #[[0,0],[0,1],[1,0],[0,-1],[-1,0],[0,2],[2,0],[0,-2],[-2,0],[0,3],[3,0],[0,-3],[-3,0],[1,1],[-1,1],[-1,-1],[1,-1]]
+                [
+                    [[0,0]],
+                    [[0,0],[0,1],[1,0],[0,-1],[-1,0]],
+                    [[0,0],[0,1],[1,0],[0,-1],[-1,0],[0,2],[2,0],[0,-2],[-2,0]],
+                    [[0,0],[0,1],[1,0],[0,-1],[-1,0],[0,2],[2,0],[0,-2],[-2,0],[0,3],[3,0],[0,-3],[-3,0],[1,1],[-1,1],[-1,-1],[1,-1]] 
         
-                    #],
+                    ]#,
                 # green stages 0 to 3
                 #[
                     #],
                 # blue stages 0 to 3
                 #[
                     #]
+                # cyan stages 0 to 3
+                #[
+                    #]
+                # magenta stages 0 to 3
+                #[
+                    #]
+                # yellow stages 0 to 3
+                #[
+                    #]
+                ]
+        speed_mods = [
+                # red stages 0 to 3
+                #[
+                    #]
+                # green stages 0 to 3
+                #[
+                    #]
+                # blue stages 0 to 3
+                #[
+                    #]
+                # cyan stages 0 to 3
+                #[
+                    #]
+                # magenta stages 0 to 3
+                #[
+                    #]
+                # yellow stages 0 to 3
+                #[
+                    #]
+                ]
+        global_index = default_colors.index(self.color)
+        pattern = patterns[global_index][self.stage]
+        # speed = speed_mods[global_index][self.stage]
 
-                #]
-        #global_index = default_colors.index(self.color)
-        #pattern = patterns[global_index][self.stage]
-
-        #for point in pattern:
+        for point in pattern:
             #print point
-            #self.points.append([(self.x + point[0] + img_size[0])%img_size[0],
-                #(self.y + point[1] + img_size[1])])
-        if self.stage == 0:
-            self.points.append([(self.x + img_size[0])%img_size[0],self.y + img_size[1]%img_size[1]])
-        #print self.points
+            self.points.append([(self.x + point[0] + img_size[0])%img_size[0],
+                (self.y + point[1] + img_size[1])])
+        #if self.stage == 0:
+            #self.points.append([(self.x + img_size[0])%img_size[0],self.y + img_size[1]%img_size[1]])
+        print self.points
 
          
